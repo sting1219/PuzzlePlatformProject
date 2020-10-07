7d6747a681d6b3048e1c27fd415dee7d8b7671b0
sting1219 <sting1219@users.noreply.github.com>
2020-10-07 12:16:35 +0900

    test send

--- PuzzlePlatform/Source/PuzzlePlatform/PlatformTrigger.h
+++ PuzzlePlatform/Source/PuzzlePlatform/PlatformTrigger.h
@@ -16,6 +16,7 @@ public:
 	// Sets default values for this actor's properties
 	APlatformTrigger();
 
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
